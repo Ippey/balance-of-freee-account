4015c2c033b89cbba1c653cfe1f2f2389d62e337
Ippei Sumida <ippey.s@gmail.com>
2018-07-12 16:45:52 +0900

    adjust

--- balence-of-freee-account.php
+++ balence-of-freee-account.php
@@ -20,7 +20,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with "Balance of freee account". If not, see https://unplat.info/.
+along with "Balance of freee account". If not, see https://www.gnu.org/licenses/gpl-2.0.html.
  */
 
 namespace Ippey\BalanceOfFreeAccount;
