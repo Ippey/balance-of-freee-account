7c61e0ea0ca44339b1c6150ff7212bb13031283e
Ippei Sumida <ippey.s@gmail.com>
2018-07-12 09:17:46 +0900

    add license and readme

--- balence-of-freee-account.php
+++ balence-of-freee-account.php
@@ -1,13 +1,26 @@
 <?php
 /**
- * Plugin Name:  Balance of Freee Account
- * Plugin URI:   https://developer.wordpress.org/plugin/balance-of-freee-account/
- * Description:  Show your balance of freee account on dashboard.
- * Version:      1
- * Author:       Ippei Sumida
- * Author URI:   https://unplat.info
- * License:      GPL-2.0-or-later
- * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ Plugin Name:  Balance of freee account
+ Plugin URI:   https://developer.wordpress.org/plugin/balance-of-freee-account/
+ Description:  Show your balance of freee account on dashboard.
+ Version:      1
+ Author:       Ippei Sumida
+ Author URI:   https://unplat.info
+ License:      GPL-2.0-or-later
+ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+
+ "Balance of freee account" is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+"Balance of freee account" is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with "Balance of freee account". If not, see https://unplat.info/.
  */
 
 namespace Ippey\BalanceOfFreeAccount;
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Balance of freee account ===
+Contributors: ippei-sumida
+Tags: accounting, freee
+Requires at least: 4.9
+Tested up to: 4.9.7
+Stable tag: 4.9
+Requires PHP: 5.6
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Show your freee's balance on dashboard.
+
+== Description ==
+
+freee is cloud accounting software. This plugin shows your balance of freee on dashboard.
+This is the long description.  No limit, and you can use Markdown (as well as in the following sections).
+
+freee: https://www.freee.co.jp/
+
+== Installation ==
+
+1. Install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress.
+1. Use the Settings->Balance of freee account to configure the plugin
+1. Access /wp-admin/
+
+
+== Frequently Asked Questions ==
+
+= A question that someone might have =
+
+== Screenshots ==
+
+== Changelog ==
+
+= 0.1 =
+* New release
+
+== Upgrade Notice ==
+
+== Arbitrary section ==
